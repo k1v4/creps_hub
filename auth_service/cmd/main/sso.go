f396145244af50ae3cf54f7108e321d9008dfdf1
Kirill Ivanchin <chvik1@yandex.ru>
2024-12-31 20:47:14 +0500

    add logger

---
+++ auth_service/cmd/main/sso.go
@@ -0,0 +1,21 @@
+package main
+
+import "context"
+
+func main() {
+	ctx := context.Background()
+	//TODO logger
+
+	//TODO config
+
+	//TODO storage
+
+	//TODO repository
+
+	//TODO service
+
+	//TODO server
+
+	//TODO graceful shutdown
+
+}
---
+++ auth_service/go.mod
@@ -0,0 +1,9 @@
+module auth_service
+
+go 1.23.2
+
+require (
+	github.com/k1v4/protos v0.0.1 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+)
