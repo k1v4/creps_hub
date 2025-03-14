d2e707c5ea50ad6ae82d2b09d4e19441d56692f6
Kirill Ivanchin <chvik1@yandex.ru>
2025-03-14 22:01:57 +0300

    add pkg files

---
+++ release_service/internal/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+	"release_service/pkg/DataBase/postgres"
+)
+
+type Config struct {
+	postgres.DBConfig
+
+	RestServerPort int `env:"REST_SERVER_PORT" env-description:"rest server port" env-default:"8080"`
+}
+
+func MustLoadConfig() *Config {
+	err := godotenv.Load(".env") // Явно указываем путь
+	if err != nil {
+		panic(err)
+	}
+
+	cfg := Config{}
+	err = cleanenv.ReadEnv(&cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	return &cfg
+}
