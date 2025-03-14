92a3a3925487591f461a2c98da70774c88f59da4
Kirill Ivanchin <chvik1@yandex.ru>
2025-03-15 00:58:03 +0300

    finish

--- release_service/internal/controller/http/v1/releases.go
+++ release_service/internal/controller/http/v1/releases.go
@@ -9,6 +9,7 @@ import (
 	"release_service/pkg/logger"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type releasesRoutes struct {
@@ -27,6 +28,12 @@ func newReleasesRoutes(handler *echo.Group, t usecase.IReleaseUseCase, l logger.
 
 	// PUT /api/v1/releases/{id}
 	handler.PUT("/releases/:id", r.updateRelease)
+
+	// GET /api/v1/releases
+	handler.GET("/releases", r.getReleasesByMonth)
+
+	// POST /api/v1/releases
+	handler.POST("/releases", r.addRelease)
 }
 
 func (r *releasesRoutes) getRelease(c echo.Context) error {
@@ -86,7 +93,7 @@ func (r *releasesRoutes) deleteRelease(c echo.Context) error {
 		return fmt.Errorf("%s: %s", op, err)
 	}
 
-	return c.JSON(http.StatusOK, entity.DeleteRequest{
+	return c.JSON(http.StatusOK, entity.DeleteResponse{
 		IsDeleted: isDeleted,
 	})
 }
@@ -95,4 +102,99 @@ func (r *releasesRoutes) updateRelease(c echo.Context) error {
 	const op = "v1.updateRelease"
 
 	ctx := c.Request().Context()
+
+	releaseId := c.Param("id")
+
+	if len(strings.TrimSpace(releaseId)) == 0 {
+		errorResponse(c, http.StatusBadRequest, "bad request")
+
+		return fmt.Errorf("%s: %s", op, "releaseID is required")
+	}
+
+	releaseIdInt, err := strconv.Atoi(releaseId)
+	if err != nil {
+		errorResponse(c, http.StatusBadRequest, "bad request")
+
+		return fmt.Errorf("%s: %s", op, err)
+	}
+
+	u := new(entity.UpdateRequest)
+	if err = c.Bind(u); err != nil {
+		errorResponse(c, http.StatusInternalServerError, "internal error")
+
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	layout := "2006-01-02"
+
+	date, err := time.Parse(layout, u.ReleaseDate)
+	if err != nil {
+		errorResponse(c, http.StatusBadRequest, "bad request")
+
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	release, err := r.t.UpdateRelease(ctx, releaseIdInt, u.Name, date)
+	if err != nil {
+		errorResponse(c, http.StatusInternalServerError, "internal error")
+
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return c.JSON(http.StatusOK, release)
+}
+
+func (r *releasesRoutes) getReleasesByMonth(c echo.Context) error {
+	const op = "v1.getReleasesByMonth"
+
+	ctx := c.Request().Context()
+
+	month := c.QueryParam("month")
+	if len(month) == 0 {
+		errorResponse(c, http.StatusBadRequest, "bad request")
+
+		return fmt.Errorf("%s: %s", op, "month is required")
+	}
+
+	byMonth, err := r.t.GetReleasesByMonth(ctx, month)
+	if err != nil {
+		errorResponse(c, http.StatusInternalServerError, "internal error")
+
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return c.JSON(http.StatusOK, byMonth)
+}
+
+func (r *releasesRoutes) addRelease(c echo.Context) error {
+	const op = "v1.AddRelease"
+
+	ctx := c.Request().Context()
+
+	u := new(entity.AddRequest)
+	if err := c.Bind(u); err != nil {
+		errorResponse(c, http.StatusInternalServerError, "internal error")
+
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	layout := "2006-01-02"
+
+	date, err := time.Parse(layout, u.ReleaseDate)
+	if err != nil {
+		errorResponse(c, http.StatusBadRequest, "bad request")
+
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	releaseId, err := r.t.AddRelease(ctx, u.Name, date)
+	if err != nil {
+		errorResponse(c, http.StatusInternalServerError, "internal error")
+
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return c.JSON(http.StatusOK, entity.AddResponse{
+		ReleaseId: releaseId,
+	})
 }
