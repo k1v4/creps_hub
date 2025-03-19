6a6b96a1c46c848576d4ec99b0919a5fd83f106c
Kirill Ivanchin <chvik1@yandex.ru>
2025-03-19 10:43:27 +0300

    add CORS

--- article_service/internal/controller/http/v1/router.go
+++ article_service/internal/controller/http/v1/router.go
@@ -12,6 +12,12 @@ func NewRouter(handler *echo.Echo, l logger.Logger, t usecase.IArticleService) {
 	// Middleware
 	handler.Use(middleware.Logger())
 	handler.Use(middleware.Recover())
+	handler.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"http://localhost:3000"},                                                                // Разрешить запросы с этого origin
+		AllowMethods:     []string{echo.GET, echo.PUT, echo.POST, echo.DELETE, echo.OPTIONS},                               // Разрешенные методы
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization}, // Разрешенные заголовки
+		AllowCredentials: true,                                                                                             // Разрешить передачу кук и заголовков авторизации
+	}))
 
 	handler.GET("/api/article/health", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{"status": "ok"})
